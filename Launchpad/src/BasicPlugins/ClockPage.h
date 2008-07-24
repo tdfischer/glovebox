e1f8a1b570605906438c2f3b5ecbdfb0d46c1a1c
Trever Fischer <wm161@wm161.net>
2008-07-24 19:18:34 -0400

    Added copyright, README, and licensing informations.

--- Launchpad/src/BasicPlugins/ClockPage.h
+++ Launchpad/src/BasicPlugins/ClockPage.h
@@ -1,6 +1,25 @@
 #ifndef CLOCKPAGE_H
 #define CLOCKPAGE_H
 
+/**
+ * Copyright (C) 2008 Trever Fischer <wm161@wm161.net>
+ *
+ * This file is part of Glovebox.
+ *
+ *  Glovebox is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Glovebox is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Glovebox.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #include <LaunchpadPage.h>
 
 class ClockPage : public LaunchpadPage
--- Launchpad/src/InvalidService.h
+++ Launchpad/src/InvalidService.h
@@ -1,3 +1,25 @@
+#ifndef INVALIDSERVICE_H
+#define INVALIDSERVICE_H
+
+/**
+ * Copyright (C) 2008 Trever Fischer <wm161@wm161.net>
+ *
+ * This file is part of Glovebox.
+ *
+ *  Glovebox is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Glovebox is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Glovebox.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #include "LaunchpadService.h"
 
 class InvalidService : public LaunchpadService
@@ -6,3 +28,5 @@ class InvalidService : public LaunchpadService
   void requestUpdate(const QString &key) {}
   void start() { stop(); }
 };
+
+#endif
