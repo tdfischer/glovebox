b2936198465b0a1d48d538797d3546f99f0345b4
Trever Fischer <wm161@wm161.net>
2008-08-19 13:44:07 -0400

    Switch the layout of the main page to a QSplitter instead of docking the page selector. Hide new docks that aren't in the focused page

--- Launchpad/src/Launcher.cpp
+++ Launchpad/src/Launcher.cpp
@@ -2,10 +2,11 @@
 
 #include <QStackedWidget>
 #include <QDockWidget>
-#include <QDebug>
+#include <QtDebug>
 #include <QDesktopWidget>
 #include <QHBoxLayout>
 #include <QSizePolicy>
+#include <QSplitter>
 
 #include "LaunchpadApp.h"
 #include "PageListModel.h"
@@ -20,14 +21,23 @@ Launcher::Launcher(QWidget* parent)
   m_pager = new PageManager(this);
   resize(qApp->desktop()->screenGeometry().size());
   m_pages = new QStackedWidget(this);
-  m_pageBar = new QDockWidget(this);
-  m_pageChooser = new QListView(m_pageBar);
-  setCentralWidget(m_pages);
+  //m_pageBar = new QDockWidget(this);
+  //m_pageChooser = new QListView(m_pageBar);
+  m_pageChooser = new QListView(this);
+  //setCentralWidget(m_pages);
+  
+  QSplitter* split = new QSplitter(this);
+  split->addWidget(m_pageChooser);
+  split->addWidget(m_pages);
+  split->setCollapsible(0, true);
+  split->setCollapsible(1, false);
+  split->setHandleWidth(25);
+  setCentralWidget(split);
 
-  m_pageBar->setFeatures(QDockWidget::DockWidgetMovable | QDockWidget::DockWidgetClosable);
-  m_pageBar->setWindowTitle(tr("Pages"));
-  m_pageBar->setWidget(m_pageChooser);
-  addDockWidget(Qt::LeftDockWidgetArea, m_pageBar);
+  //m_pageBar->setFeatures(QDockWidget::DockWidgetMovable | QDockWidget::DockWidgetClosable);
+  //m_pageBar->setWindowTitle(tr("Pages"));
+  //m_pageBar->setWidget(m_pageChooser);
+  //addDockWidget(Qt::LeftDockWidgetArea, m_pageBar);
 
   m_pageChooser->setResizeMode(QListView::Adjust);
   m_pageChooser->setMovement(QListView::Snap);
@@ -38,10 +48,10 @@ Launcher::Launcher(QWidget* parent)
   m_pageChooser->setFlow(QListView::TopToBottom);
   m_pageChooser->setUniformItemSizes(true);
   m_pageChooser->setSpacing(5);
-  m_pageBar->resize(QSize(64,64));
+  //m_pageBar->resize(QSize(64,64));
   m_pages->resize(qApp->desktop()->screenGeometry().size());
 
-  connect(m_pageBar, SIGNAL(dockLocationChanged(Qt::DockWidgetArea)), this, SLOT(updatePageBarDirection(Qt::DockWidgetArea)));
+  //connect(m_pageBar, SIGNAL(dockLocationChanged(Qt::DockWidgetArea)), this, SLOT(updatePageBarDirection(Qt::DockWidgetArea)));
   connect(m_pageChooser->selectionModel(), SIGNAL(currentChanged(const QModelIndex&, const QModelIndex&)), this, SLOT(switchPage(const QModelIndex&, const QModelIndex&)));
 
   connect(m_pager->model(), SIGNAL(pageAdded(LaunchpadPage*)), this, SLOT(pageAdded(LaunchpadPage*)));
@@ -75,8 +85,11 @@ Launcher::pageAdded(LaunchpadPage* page)
 void
 Launcher::addDock(QDockWidget* widget)
 {
-  //TODO: Hide docks from pages that aren't the top page
   addDockWidget(Qt::RightDockWidgetArea, widget);
+  widget->hide();
+  foreach(QDockWidget* dock, m_pageChooser->selectionModel()->currentIndex().data(PageListModel::DockRole).value<QList<QDockWidget*> >()) {
+    dock->show();
+  }
 }
 
 void
