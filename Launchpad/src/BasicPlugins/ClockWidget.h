b599b1160d97123bbc016497df3ef78d7e42892b
Trever Fischer <wm161@wm161.net>
2008-09-25 20:56:56 -0400

    A snazzy looking clock widget.

--- Launchpad/src/BasicPlugins/ClockWidget.h
+++ Launchpad/src/BasicPlugins/ClockWidget.h
@@ -3,6 +3,7 @@
 
 #include "DashWidget.h"
 #include <QGraphicsTextItem>
+#include <QGraphicsEllipseItem>
 #include <Service.h>
 #include <ServiceManager.h>
 
@@ -15,13 +16,17 @@ class ClockWidget : public DashWidget {
   public:
     ClockWidget(QGraphicsItem* parent = 0);
     void init();
+    QRectF boundingRect() const;
     
   public slots:
     void dataUpdated(const QString &key, const QVariant &data);
     
   private:
     Service* timeService;
-    QGraphicsTextItem* m_label;
+    QGraphicsEllipseItem* m_clock;
+    QGraphicsPolygonItem* m_minutes;
+    QGraphicsPolygonItem* m_seconds;
+    QGraphicsPolygonItem* m_hours;
 };
 
 #endif
